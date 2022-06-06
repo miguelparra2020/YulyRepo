b547433761a7ed1812bb3f0c8d977fe23a31901b
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2022-06-05 22:59:57 -0500

    Updated

--- ciclos/50_ejercicios_Ciclos/ejercicio_6.py
+++ ciclos/50_ejercicios_Ciclos/ejercicio_6.py
@@ -1,12 +1,36 @@
 # 6. Leer un número entero de tres dígitos y mostrar todos los enteros comprendidos entre 1
 # y cada uno de los dígitos.
 try:
-    numero_1 = int(input("Por favor ingresar el primer numero entero"))
-    numero_2 = int(input("Por favor ingresar el segundo numero entero"))
+    numero_1 = int(input("Por favor ingresar un numero entero de tres digitos"))
 
-    for i in range(numero_1, numero_2+1):
-        num_1 = i%10
-        if num_1==4:
-            print("estos numeros terminan en 4, el:    ", i)
+    if (numero_1>=100 and numero_1<=999):
+        num_1 = numero_1//100
+        num_2 = (numero_1//10)%10
+        num_3 = numero_1%10 
+        print("Del numero ingresado tienen los siguientes digitos  ",num_1,num_2, num_3)
+        for i in range(1, num_1+1):
+            print("Del primer digito:     ", i)
+        print("")
+        for j in range(1, num_2+1):
+            print("Del segundo digito:     ", j)
+        print("")
+        for k in range(1, num_3+1):
+            print("Del tercer digito:     ", k)
+    elif (numero_1>=-999 and numero_1<=-100):
+        num_1_positivo = numero_1*(-1)
+        num_1 = num_1_positivo//100
+        num_2 = (num_1_positivo//10)%10
+        num_3 = num_1_positivo%10 
+        print("Del numero ingresado tienen los siguientes digitos  ",num_1*(-1),num_2*(-1), num_3*(-1))
+        for i in range(num_1*(-1),0):
+            print("Del primer digito:     ", i)
+        print("")
+        for j in range(num_2*(-1),0):
+            print("Del segundo digito:     ", j)
+        print("")
+        for k in range(num_3*(-1), 0):
+            print("Del tercer digito:     ", k)
+    else:
+        ("El numero ingresado no es de tres digitos")
 except ValueError:
     print("Error")
